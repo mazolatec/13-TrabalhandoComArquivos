75fb2b777303ca31d6429e7269cab486c3448f25
mazolatec <mazolatec@gmail.com>
2022-11-11 11:46:54 -0300

    Add 07-DirectoryDirectoryInfo com README

---
+++ 13-TrabalhandoComArquivosBasicos/07-DirectoryDirectoryInfo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07_DirectoryDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\PASTA-TESTE";
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                //var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                //Directory.CreateDirectory(path+"\\newmaza"); 
+                Directory.CreateDirectory(path + @"\newfoldermaza");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+}
---
+++ 13-TrabalhandoComArquivosBasicos/07-DirectoryDirectoryInfo/README.txt
@@ -0,0 +1,14 @@
+﻿Directory, DirectoryInfo
+• Namespace System.IO
+• Operações com pastas (create, enumerate, get files, etc.).
+• Directory
+• static members (simple, but performs security check for each operation)
+• https://msdn.microsoft.com/en-us/library/system.io.directory(v=vs.110).aspx
+• DirectoryInfo
+• instance members
+• https://msdn.microsoft.com/en-us/library/system.io.directoryinfo(v=vs.110).aspx
+
+Demo
+• Vamos listar as pastas a partir de uma pasta informada
+• Vamos listar os arquivos a partir de uma pasta informada
+• Vamos criar uma pasta
